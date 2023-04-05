345eda754448fcbabd4fa333f1e4f64a4e9160f8
Jiaming-Z <tiger080710263003@berkeley.edu>
2023-04-05 13:56:00 -0700

    added calibration algo and created subscribers in many nodes

--- pt_selection_pkg/pt_selection_pkg/rviz_pt_collection.py
+++ pt_selection_pkg/pt_selection_pkg/rviz_pt_collection.py
@@ -1,1 +1,59 @@
 #collect 5-10 pt clouds from rosbag, merge them, and then publish the merged pointcloud (similar to calib_test_node) to RVIZ
+
+class pt_collection_node(Node):
+
+    def __init__(self):
+        super().__init__('calibration_subscriber_node')
+
+        self.qos_profile =  QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1,
+        )
+
+        self.latest_im = None
+        self.latest_pc = None
+
+        # Pointcloud buffer
+        self.buffer_front = []
+        self.buffer_left = []
+        self.buffer_right = []
+        self.counter = 0
+        self.threshold = 5
+        
+        
+        # Subscribe to pcd file
+        self.pcd_file = self.create_subscription(
+            sensor_msgs.PointCloud2,
+            "/luminar_front_points",  # Subscribes from front lidar
+            self.sub_callback_pcd,
+            self.qos_profile)
+        self.pcd_file  # Prevent unused variable warning
+
+        self.merged_pcd_publisher = self.create_publisher(
+            sensor_msgs.PointCloud2,
+            "/merged_pointcloud",
+            rclpy.qos.qos_profile_sensor_data)
+
+
+    def sub_callback_pcd(self, PointCloud2):
+
+        #calibration_subscriber_node.glob_pcd_file = None
+        self.latest_pc = None
+        gen2 = read_points(PointCloud2, field_names=['x', 'y', 'z'], skip_nans=True) # returns a pointcloud Generator
+        #gen = pointcloud2_to_pcd_file.read_points(PointCloud2, skip_nans=True) # returns a pointcloud Generator
+        #self.pc_total = np.array(list(gen2))
+        self.latest_pc = np.array(list(gen2))
+        file_path = '/home/jiamingzhang/temp.pkl'
+        with open(file_path, 'wb') as f:
+            pickle.dump(self.latest_pc, f)
+        self.buffer_front.append(PointCloud2)
+        
+    
+    # merge 5 point clouds, publish them
+    def merged_publisher_callback(self, PointCloud2):
+        self.merged_pcd = 
+        for i in range(5):
+            self.merged_pcd = 
+        
+        self.merged_pcd_publisher.publish(merged_pcd)
--- pt_selection_pkg/pt_selection_pkg/rviz_pt_selection.py
+++ pt_selection_pkg/pt_selection_pkg/rviz_pt_selection.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, Marker
 from geometry_msgs.msg import Point
+import numpy as np
 
 ### point selection using the topic : clicking points: node subscribe to /clicked_point topic to rviz node, and in rviz "publish point", click to see point, record clicked point
 
@@ -11,7 +12,74 @@ class InteractiveMarkerNode(Node):
     def __init__(self):
         super().__init__('interactive_marker_node')
 
+        self.qos_profile =  QoSProfile(
+            reliability=QoSReliabilityPolicy.BEST_EFFORT,
+            history=QoSHistoryPolicy.KEEP_LAST,
+            depth=1,
+        )
+        self.pcd_file = self.create_subscription(
+            sensor_msgs.PointCloud2,
+            "/clicked_point",  # Subscribes from clicked point in rviz
+            self.sub_callback_clicked_pt,
+            self.qos_profile)
+
+        self.selected_pts = np.array() # in [u,v,x,y,z] format, select 10 points
         
+        
+    def sub_callback_clicked_pt(self):
+        
+
+    # write R and t into txt file
+    def write_R_t_into_file(self):
+        R, t = self.calibration_algorithum()
+        f = open('R_t.txt', 'w')
+        f.write(R)
+        f.write(t)
+        f.close()
+
+    # get R and t
+    def calibration_algorithum(self):
+        def make_A(input2):
+            A_lst_T = [] # use list for flexibility
+            for i in range(len(input2)):
+                n = input2[i]
+                I = np.dot(K_inv, np.array([n[0], n[1], 1])) #[X',Y',1]
+                A_lst_T.append([0*n[2], 0*n[3], 0*n[4], 0, -1*n[2], -1*n[3], -1*n[4], -1, I[1]*n[2], I[1]*n[3], I[1]*n[4], I[1]])
+                A_lst_T.append([1*n[2], 1*n[3], 1*n[4], 1, 0*n[2], 0*n[3], 0*n[4], 0, -I[0]*n[2], -I[0]*n[3], -I[0]*n[4], -I[0]])
+            
+            A_T = np.array(A_lst_T)
+            A = np.transpose(A_T)
+            return A
+        
+        A = make_A(self.selected_pts)
+
+        def get_raw_Rt(A):
+            U, sig, VT = np.linalg.svd(A)
+
+            rt_vec = U[:,11]
+            R_raw = np.array([[rt_vec[0], rt_vec[1], rt_vec[2]],
+                        [rt_vec[4], rt_vec[5], rt_vec[6]],
+                        [rt_vec[8], rt_vec[9], rt_vec[10]]])
+            t_raw = np.array([rt_vec[3], rt_vec[7], rt_vec[11]])
+            return R_raw, t_raw
+        
+        R_raw, t_raw = get_raw_Rt(A)
+
+        def get_real_Rt(R_raw, t_raw):
+            UR, sigR, VTR = np.linalg.svd(R_raw)
+            D = np.array([[1,0,0],
+                        [0,1,0],
+                        [0,0,1]])
+            R = np.dot(UR, np.dot(D, VTR))
+
+            s = (sigR[0] + sigR[1] + sigR[2])/3
+            t = t_raw/s
+            return R, t
+
+        R, t = get_real_Rt(R_raw, t_raw)
+        return R, t
+
+
 
 
 def main(args=None):
